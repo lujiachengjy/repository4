a531651c320025ef0270630ccd4dd706dd161258
鲁嘉城 <3192435352@qq.com>
2021-02-01 21:42:03 +0800

    嘿嘿

---
+++ git-test01/src/com/ljc/Product.java
@@ -0,0 +1,11 @@
+package com.ljc;
+
+public class Product {
+    public static void main(String[] args) {
+        System.out.println("hello git!!");
+        System.out.println("hello git!!");
+        System.out.println("hello git!!");
+        System.out.println("hello git!!");
+        System.out.println("hello git!!");
+    }
+}
